8d4f3a58991e32683b024b387e327f51fafe6e18
Ninh Duong <ninhduong25nd@gmail.com>
2022-09-22 09:48:10 +0700

    "update"

--- src/main/java/com/example/nexusdemo/MySum.java
+++ src/main/java/com/example/nexusdemo/MySum.java
@@ -1,6 +1,15 @@
 package com.example.nexusdemo;
 
 public class MySum {
+    public static void hello(String[] names){
+        System.out.println("Hello world");
+        System.out.println("Members:");
+        for (String i :
+                names) {
+            System.out.println("{"+i+"},");
+        }
+    }
+
     public static int add(int a, int b){
         return a+b;
     }
